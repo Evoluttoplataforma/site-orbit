558c159012fdd335d671eb6451a69ef84ebf8297
Marco Oliveira <marco.oliveira@orbitgestao.com.br>
2026-06-19 10:58:23 -0300

    fix: separa footer do wrapper do artigo para corrigir tamanho da logo

--- orbit-next/src/app/historias/[slug]/page.tsx
+++ orbit-next/src/app/historias/[slug]/page.tsx
@@ -220,7 +220,6 @@ export default async function StoryPage({ params }: { params: Promise<{ slug: st
         </aside>
       </div>
     </div>
-    ${footerHTML}
   `;
 
   return (
@@ -228,6 +227,7 @@ export default async function StoryPage({ params }: { params: Promise<{ slug: st
       <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} />
       <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(breadcrumb) }} />
       <div dangerouslySetInnerHTML={{ __html: articleHTML }} />
+      <div dangerouslySetInnerHTML={{ __html: footerHTML }} />
     </>
   );
 }
